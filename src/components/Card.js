1579e59a1d1001b777cbd2ecb6de31320117b512
Amrit Singh <amritsinghy53@gmail.com>
2022-11-12 10:12:32 +0530

    added

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+//https://tse1.mm.bing.net/th?id=OIP.33CwBYkmnMfpA9Djup22JwHaHa&pid=Api&P=0
+function card() {
+  return (
+    <div>card</div>
+  )
+}
+
+export default card
