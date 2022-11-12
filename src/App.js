415359fe2499bb1f5370aa33ce1ba6d0bb61ad46
Amrit Singh <amritsinghy53@gmail.com>
2022-11-12 16:51:04 +0530

    REACT_CLASS8

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
+import "./App.css";
+import Card from "./components/Card";
 
-import './App.css';
-
+const data = [
+ {
+  image: "https://www.profitconfidential.com/wp-content/uploads/2020/11/logo-of-apple-inc-on-a-mac-store-hong-kong-2018-03-14_t20_6Yk222.jpg",
+  prodName: "Apple",
+  _id : 221522,
+  desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto reiciendis maxime laborum sint nemo, praesentium possimus minus sit tenetur at cum, iusto inventore nulla rerum expedita amet. Porro recusandae esse laborum doloremque fugit corporis?",
+  // image: <img src="/src/images/apple.jpg" />
+ },
+ {
+  image: "http://www.aymen-loukil.com/wp-content/uploads/2017/08/Google-images-badges-on-search-min.png",
+  prodName: "Google",
+  _id : 985252,
+  desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto reiciendis maxime laborum sint nemo, praesentium possimus minus sit tenetur at cum, iusto inventore nulla rerum expedita amet. Porro recusandae esse laborum doloremque fugit corporis?",
+ },
+ 
+ {
+  image: "https://images.hindustantimes.com/tech/rf/image_size_960x540/HT/p2/2019/04/05/Pictures/books-photo-retail-store-seen-amazon-amazon_16cf7722-578e-11e9-8bc0-bee180be535f.jpg",
+  prodName: "Amazon",
+  _id : 45632565,
+  desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto reiciendis maxime laborum sint nemo, praesentium possimus minus sit tenetur at cum, iusto inventore nulla rerum expedita amet. Porro recusandae esse laborum doloremque fugit corporis?",
+ },
+ {
+  image: "https://tse4.mm.bing.net/th?id=OIP.7k2mPJJ5xLVI1lfzQMAqDAHaDg&pid=Api&P=0",
+  prodName: "Microsoft",
+  _id : 545322,
+  desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto reiciendis maxime laborum sint nemo, praesentium possimus minus sit tenetur at cum, iusto inventore nulla rerum expedita amet. Porro recusandae esse laborum doloremque fugit corporis?",
+ },
+];
 function App() {
-  return (<p>Hello Amrit Singy</p>);
+  const title="DREAM COMPANIES"
+  return (
+    
+    <div className="container">
+      <h1>{title}</h1> {/*in javascipt comment is written like this and if we want to include JS then use {}*/ }
+      <br />
+      <div className="row">
+        {
+          data.map((val,ind) => {
+            return(
+              <div className="col-12 col-sm-6 col-md-4">
+                <Card values={val}/>
+              </div>
+            )
+            
+          })
+        }
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,49 @@
-import React from 'react'
+// import React from 'react'
 //https://tse1.mm.bing.net/th?id=OIP.33CwBYkmnMfpA9Djup22JwHaHa&pid=Api&P=0
-function card() {
-  return (
-    <div>card</div>
-  )
+//  const Card = (props) => {
+  // console.log(props.values)
+  
+  // PROPS DESTRUCTURING
+//   const {_id,prodName,image,desc} = props.values;
+//   return (
+//     <div class="card" >
+//   <img src={image} class="card-img-top" alt="..." />
+//   <div class="card-body">
+//     <h5 class="card-title">{prodName}</h5>
+//     <p class="card-text">{desc}</p>
+//     <button class="btn btn-primary">Apply</button> 
+//   </div>
+// </div>
+//   );
+// }
+
+
+// export default Card
+
+import React, { Component } from 'react';
+
+ class Card extends Component {
+  render() {
+    console.log("This is class component and we do not have to pass any argument",this.props);
+
+    const {_id,prodName,image,desc} = this.props.values;
+    this.State={
+      btnText : "JOB_ID  ::  "+_id ,
+      btnNew : "APPLY"
+    }
+    
+    return (
+      <div class="card" >
+    <img src={image} class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">{prodName}</h5>
+      <p class="card-text">{desc}</p>
+      <button class="btn btn-primary me-4">{this.State.btnText}</button> 
+      <button class="btn btn-primary">{this.State.btnNew}</button> 
+    </div>
+  </div>
+    );
+  }
 }
 
-export default card
+export default Card
